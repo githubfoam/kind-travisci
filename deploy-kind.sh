201e92f8ace4a40365a5462eb78b91277e27efa7
githubfoam <lequestionis@gmail.com>
2020-06-17 16:01:55 +0300

    Yeah.

--- deploy-kind.sh
+++ deploy-kind.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "=============================deploy kind============================================================="
-- docker version
-- curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64
-- chmod +x ./kind
-- mv ./kind /usr/local/bin/kind
-- kind get clusters #see the list of kind clusters
-- kind get clusters
-- kubectl config get-contexts #kind is prefixed to the context and cluster names, for example: kind-istio-testing
+docker version
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
+kind get clusters #see the list of kind clusters
+kind get clusters
+kubectl config get-contexts #kind is prefixed to the context and cluster names, for example: kind-istio-testing
 echo "=============================deploy kind============================================================="
